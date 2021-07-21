10326a4b9d0e3db8f9a541ac3ed38e66f000d3ca
Andres Molestina <zocrates@gmail.com>
2021-07-21 16:02:34 -0400

    figuring out how to add new employee on user imput

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console_table');
-const Choice = require('inquirer/lib/objects/choice');
 
+
+// questions for adding new role had to put into array because it wouldnt work within the prompt for some reason
+const addRoleQuestions = [
+    {
+      name: 'roleName',
+      type: 'input',
+      message: "What is the new role?",
+      validate: function validateRoleName(roleName){
+        return roleName !== '';
+      },
+    },
+    {
+      name: 'roleSalary',
+      type: 'input',
+      message: "What is the salary for this new role?",
+      validate: function validateRoleSalary(roleSalary){
+        return roleSalary !== '';
+      },
+    },
+    {
+      name: 'roleDept',
+      type: 'input',
+      message: "What department does this role fall under?",
+      validate: function validateRoleDept(roleDept){
+        return roleDept !== '';
+      },
+    },
+  ];
+  
+  // questions for adding new employee had to put into array because it wouldnt work within the prompt for some reason
+  const addEmpQuestions = [
+    {
+      name: 'firstName',
+      type: 'input',
+      message: "What is the employees first name?",
+      validate: function validateFirstName(FirstName){
+        return FirstName !== '';
+      },
+    },
+    {
+      name: 'lastName',
+      type: 'input',
+      message: "What is the employees last name?",
+      validate: function validateLastName(LastName){
+        return LastName !== '';
+      },
+    },
+    {
+      name: 'empRole',
+      type: 'input',
+      message: "What role will this employee be performing?",
+      validate: function validateEmpRole(EmpRole){
+        return EmpRole !== '';
+      },
+    },
+    {
+      name: 'empManager',
+      type: 'input',
+      message: "Who is this employees manager?",
+    },
+  ];
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -94,33 +154,6 @@ const runQuestions = () => {
   
   };
 
-  // questions for adding new role had to put into array because it wouldnt work within the prompt for some reason
-const addRoleQuestions = [
-    {
-      name: 'roleName',
-      type: 'input',
-      message: "What is the new role?",
-      validate: function validateRoleName(roleName){
-        return roleName !== '';
-      },
-    },
-    {
-      name: 'roleSalary',
-      type: 'input',
-      message: 'What is the salary for this new role?',
-      validate: function validateRoleSalary(roleSalary){
-        return roleSalary !== '';
-      },
-    },
-    {
-      name: 'roleDept',
-      type: 'input',
-      message: "What department does this role fall under?",
-      validate: function validateRoleDept(roleDept){
-        return roleDept !== '';
-      },
-    },
-  ];
   
   // prompts the user for information on the role they wish to add and adds it to the table role
   const addRole = () => {
@@ -146,6 +179,33 @@ const addRoleQuestions = [
   
     })
 };
+
+// prompts the user for information on the employee they wish to add and adds it to the table employee
+const addEmployee = () => {
+    inquirer.prompt(addEmpQuestions)
+    .then((userAnswer) => {
+      console.log('Inserting a new employee...\n');
+      const query = connection.query(
+        'INSERT INTO employee SET ?',
+        {
+          first_name: userAnswer.firstName,
+          last_name: userAnswer.lastName,
+          role_id: userAnswer.empRole,
+          manager_id: userAnswer.empManager,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} department inserted!\n`);
+          // Call runQuestions() AFTER the INSERT completes
+          runQuestions();
+        }
+      );
+      // logs the actual query being run
+      console.log(query.sql);
+  
+    })
+  };
+  
   
   const showDepts = () => {
     const query =
